2f01860a82feee7926516d5fa779806525f18578
ChenhaoYi-n <cyin1970@usc.edu>
2024-09-07 17:05:04 -0700

    Plane-Challenge

---
+++ Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,74 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void FollowPlayerX_Start_mE73AEAF1AE3395058DDD44681F9CC519DD2692F6 (void);
+extern void FollowPlayerX_Update_m657B1EB411925E30C5169B39F524D340503E0F26 (void);
+extern void FollowPlayerX__ctor_m95820EBAEB39F06210CD082F86E5B37AFDCB3ECE (void);
+extern void PlayerControllerX_Start_m5BAE410291E1DABAC99D3F233D14AF0AA072A606 (void);
+extern void PlayerControllerX_FixedUpdate_mF6B5972ED4C7FA460F293640E0EF7A5DDE586A23 (void);
+extern void PlayerControllerX__ctor_m3FFA731641AD6FA1CE627820042B7587E98D535A (void);
+extern void Rotate_Start_mD322E77A3CF2BEF28C4DF71D3F529107F511B1FB (void);
+extern void Rotate_Update_m73D585515036D9B7AAD8336BFB8567283CE4C7E7 (void);
+extern void Rotate__ctor_m0EE5CC8EB699542BFC438DC3D547D39E442E9EE4 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[11] = 
+{
+	FollowPlayerX_Start_mE73AEAF1AE3395058DDD44681F9CC519DD2692F6,
+	FollowPlayerX_Update_m657B1EB411925E30C5169B39F524D340503E0F26,
+	FollowPlayerX__ctor_m95820EBAEB39F06210CD082F86E5B37AFDCB3ECE,
+	PlayerControllerX_Start_m5BAE410291E1DABAC99D3F233D14AF0AA072A606,
+	PlayerControllerX_FixedUpdate_mF6B5972ED4C7FA460F293640E0EF7A5DDE586A23,
+	PlayerControllerX__ctor_m3FFA731641AD6FA1CE627820042B7587E98D535A,
+	Rotate_Start_mD322E77A3CF2BEF28C4DF71D3F529107F511B1FB,
+	Rotate_Update_m73D585515036D9B7AAD8336BFB8567283CE4C7E7,
+	Rotate__ctor_m0EE5CC8EB699542BFC438DC3D547D39E442E9EE4,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[11] = 
+{
+	1116,
+	1116,
+	1116,
+	1116,
+	1116,
+	1116,
+	1116,
+	1116,
+	1116,
+	2158,
+	1116,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	11,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
